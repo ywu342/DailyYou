fe2bb77e031a731a04339dbbe390cc5aac94b5c5
ywu342 <valence931215@gmail.com>
2017-04-13 14:13:21 -0400

    initial config

--- DailyU/news/models.py
+++ DailyU/news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    cate_name = models.CharField(max_length=200)
+
+class User(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    categories = models.ForeignKey(Category, on_delete=models.CASCADE)
+
