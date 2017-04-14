b1a56b47edd781e6a9811d62e962cf0076bd24ad
Yan <yanjxx@bu.edu>
2017-04-13 22:53:28 -0400

    login

--- DailyU/news/models.py
+++ DailyU/news/models.py
@@ -2,10 +2,36 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    cate_name = models.CharField(max_length=200)
+    cate_name = models.CharField(max_length=200,primary_key=True)
+    
+    def __str__(self):
+        return self.cate_name
+    
+    class Meta:
+        ordering = ('cate_name',)
 
 class User(models.Model):
-    username = models.CharField(max_length=200)
+    username = models.CharField(max_length=200,primary_key=True)
     password = models.CharField(max_length=200)
-    categories = models.ForeignKey(Category, on_delete=models.CASCADE)
-
+    categories = models.ManyToManyField(Category)
+    
+    def addCateToUser(self, cate):
+        category = Category.objects.get(cate_name=cate)
+        self.categories.add(category)
+    
+    def rmCateFromUser(self, cate):
+        category = Category.objects.get(cate_name=cate)
+        self.categories.remove(category)
+    
+    def getUserCates(self):
+        qs = self.categories.all()
+        categories = []
+        for q in qs:
+            categories.append(q.cate_name)
+        return categories
+    
+    def __str__(self):
+        return self.username
+    
+    class Meta:
+        ordering = ('username',)
--- DailyU/news/views.py
+++ DailyU/news/views.py
@@ -1,10 +1,51 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+<<<<<<< HEAD
 from django.contrib.auth.decorators import login_required
+=======
+from .models import Category, User
+>>>>>>> 4903c1f90c35bd46f11b4d57773f789231361049
 
 # Create your views here.
 
 # The index page is not allow to any view without authentication
 @login_required(login_url="login/")
 def index(request):
-    return HttpResponse("This is the index page: Login")
+    u = User.objects.get(username="yaling")
+    u.rmCateFromUser('sports')
+    #u.addCateToUser('sports')
+    output = ','.join(u.getUserCates())
+    return HttpResponse("All cates for yaling in db: "+output)
+
+def profile(request):
+    return HttpResponse("profile page")
+
+def downloadPDF(request):
+    return HttpResponse("download pdf page")
+
+def generateNewspaper(request):
+    return HttpResponse("Newspaper page")
+
+# private helper functions to communicate with the db
+def __addCateToUser(user_name, cate):
+    u = User.objects.get(username=user_name)
+    category = Category.objects.get(cate_name=cate)
+    u.categories.add(category)
+    
+def __rmCateFromUser(user_name,cate):
+    u = User.objects.get(username=user_name)
+    category = Category.objects.get(cate_name=cate)
+    u.categories.remove(category)
+    
+def __getUserCates(user_name):
+    u = User.objects.get(username=user_name)
+    qs = u.categories.all()
+    categories = []
+    for q in qs:
+        categories.append(q.cate_name)
+    return categories
+
+def __getAllUser():
+    latest_user_list = User.objects.all()
+    output = ', '.join([u.username for u in latest_user_list])
+    return output
