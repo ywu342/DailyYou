87fa7529b2457030356caef23771379f97a52ef7
ywu342 <valence931215@gmail.com>
2017-04-13 23:05:11 -0400

    minor changes

--- DailyU/news/views.py
+++ DailyU/news/views.py
@@ -5,8 +5,9 @@ from .models import Category, User
 # Create your views here.
 def index(request):
     u = User.objects.get(username="yaling")
+    u.addCateToUser('sports')
+    print('***************test dbutils***************\n'+','.join(u.getUserCates()))
     u.rmCateFromUser('sports')
-    #u.addCateToUser('sports')
     output = ','.join(u.getUserCates())
     return HttpResponse("All cates for yaling in db: "+output)
 
@@ -18,27 +19,3 @@ def downloadPDF(request):
 
 def generateNewspaper(request):
     return HttpResponse("Newspaper page")
-
-# private helper functions to communicate with the db
-def __addCateToUser(user_name, cate):
-    u = User.objects.get(username=user_name)
-    category = Category.objects.get(cate_name=cate)
-    u.categories.add(category)
-    
-def __rmCateFromUser(user_name,cate):
-    u = User.objects.get(username=user_name)
-    category = Category.objects.get(cate_name=cate)
-    u.categories.remove(category)
-    
-def __getUserCates(user_name):
-    u = User.objects.get(username=user_name)
-    qs = u.categories.all()
-    categories = []
-    for q in qs:
-        categories.append(q.cate_name)
-    return categories
-
-def __getAllUser():
-    latest_user_list = User.objects.all()
-    output = ', '.join([u.username for u in latest_user_list])
-    return output
