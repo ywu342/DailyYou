eabf33055d90a6a7f3d45a3286ab113d44b132c1
ywu342 <valence931215@gmail.com>
2017-04-13 22:12:53 -0400

    communications with db

--- DailyU/news/models.py
+++ DailyU/news/models.py
@@ -2,10 +2,21 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    cate_name = models.CharField(max_length=200)
+    cate_name = models.CharField(max_length=200,primary_key=True)
+    
+    def __str__(self):
+        return self.cate_name
+    
+    class Meta:
+        ordering = ('cate_name',)
 
 class User(models.Model):
-    username = models.CharField(max_length=200)
+    username = models.CharField(max_length=200,primary_key=True)
     password = models.CharField(max_length=200)
-    categories = models.ForeignKey(Category, on_delete=models.CASCADE)
-
+    categories = models.ManyToManyField(Category)
+    
+    def __str__(self):
+        return self.username
+    
+    class Meta:
+        ordering = ('username',)
--- DailyU/news/views.py
+++ DailyU/news/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Category, User
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is the index page: Login")
+    __rmCateFromUser('yaling', 'sports')
+    output = ','.join(__getUserCates('yaling'))
+    return HttpResponse("All cates for yaling in db: "+output)
+
+# private helper functions to communicate with the db
+def __addCateToUser(user_name, cate):
+    u = User.objects.get(username=user_name)
+    category = Category.objects.get(cate_name=cate)
+    u.categories.add(category)
+    
+def __rmCateFromUser(user_name,cate):
+    u = User.objects.get(username=user_name)
+    category = Category.objects.get(cate_name=cate)
+    u.categories.remove(category)
+    
+def __getUserCates(user_name):
+    u = User.objects.get(username=user_name)
+    qs = u.categories.all()
+    categories = []
+    for q in qs:
+        categories.append(q.cate_name)
+    return categories
+
+def __getAllUser():
+    latest_user_list = User.objects.all()
+    output = ', '.join([u.username for u in latest_user_list])
+    return output
