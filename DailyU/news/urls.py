4b288165780311c7352f0e98c21e816fe556ffb4
Yan <yanjxx@bu.edu>
2017-04-15 22:07:59 -0400

    login with news_user table

--- DailyU/news/urls.py
+++ DailyU/news/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from . import forms
+
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login/', auth_views.login, {'template_name':'login.html', 'authentication_form':forms.LoginForm},name='login'),
+    url(r'^login/', views.login,name='login'),
     url(r'^logout/', auth_views.logout, {'next_page':'/login'},name='logout'),
+    url(r'^editcat/', views.editCategory, name='editcat')
     
 ]
--- DailyU/news/views.py
+++ DailyU/news/views.py
@@ -1,14 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,render_to_response,redirect
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
 from .models import Category, User
+import sys
 
 
 # Create your views here.
 
 # The index page is not allow to any view without authentication
-@login_required(login_url="login/")
+def getCurrentUser(request):
+    username = request.session.get('current_user',None)
+    if username is not None:
+        user = User.objects.get(username =username )
+    else:
+        user = None
+    return user
+
+def check_login(orig_func):
+    def temp_func(request):
+        if getCurrentUser(request) == None:
+            return HttpResponseRedirect("/login")
+        else:
+            return orig_func(request)
+    return temp_func
+
+
+@check_login
 def index(request):
     cur_user = request.user
     print(cur_user.username)
@@ -17,7 +37,28 @@ def index(request):
     print('***************test dbutils***************\n'+','.join(u.getUserCates()))
     u.rmCateFromUser('sports')
     output = ','.join(u.getUserCates())
-    return render(request, "home.html")
+    return render(request,"home.html")
+
+def login(request):
+    username = request.POST.get('username',False)
+    password = request.POST.get('password',False)
+    try:
+        user = User.objects.get(username=username)
+        if(str(user.password) == str(password)):
+            request.session['current_user'] = user.username
+            return render_to_response("home.html",{'user':user})
+        else:
+            return render_to_response("login.html",{'err':'login error',
+                'user':None},\
+                context_instance=RequestContext(request))
+    except:
+        e = sys.exc_info()[0]
+        print(e)
+    return render(request, "login.html")
+
+
+def editCategory(request):
+    return
 
 def downloadPDF(request):
     return HttpResponse("download pdf page")
